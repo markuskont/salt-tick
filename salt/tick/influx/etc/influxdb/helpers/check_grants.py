773956fff0cf256ccd3bf383cf4e08c63163e8d5
Markus Kont <markuskont@gmail.com>
2017-02-02 12:57:27 +0200

    WIP, checkpoint for justice

--- salt/tick/influx/etc/influxdb/helpers/check_grants.py
+++ salt/tick/influx/etc/influxdb/helpers/check_grants.py
@@ -23,35 +23,51 @@ GRNT = json.loads(ARGS.data)
 CHK = ARGS.check
 
 def show_grants(pillar):
-    dic = {}
+    current = {}
+    query = auth()
     for user, perm in pillar.items():
-        query = {
-            'u': USER,
-            'p': PASW,
-            'q': 'SHOW GRANTS FOR ' + user
-        }
+        query['q'] = 'SHOW GRANTS FOR ' + user
         req = request(query)
         ret = requests.get(req, verify=False).text
         data = json.loads(ret)
         if 'series' in data['results'][0]:
-            dic[user] = {}
+            current[user] = {}
             grants = data["results"][0]["series"][0]
             if 'values' in grants:
                 for val in grants['values']:
-                    dic[user][val[0]] = val[1]
+                    current[user][val[0]] = val[1]
             else:
-                dic[user] = None
-    return dic
+                current[user] = None
+    return current
 
 def get_missing(result, pillar):
-    diff = []
-    for item, perm in pillar.items():
-        if item not in result:
-            diff.append(item)
+    diff = {}
+    for user, grants in pillar.items():
+        #if not result[user]:
+        #    diff[user] = {}
+        if user in result:
+            if result[user] == None:
+                diff[user] = pillar[user]
+            else:
+                for db, perm in grants.items():
+                    if db in result[user]:
+                        if perm != result[user][db]:
+                            diff[user][db] = perm
+                    else:
+                        pass
+                #if db in result[user]:
+                #    pass
+                #    #print db + ' : ' + perm
+                #else:
+                #    diff[user][db] = None
+                #pass
+        else:
+            #if result[user]:
+            diff[user] = grants
+                #if result[user][db]:
+                #    if result[user][db] != perm:
+                #        print perm
     return diff
-
-def get_different(result, pillar):
-    pass
 
 def create_missing(databases):
     resp = {}
@@ -69,10 +85,16 @@ def create_missing(databases):
 def request(query):
     return 'https://' + HOST + ':8086/query?' + urllib.urlencode(query)
 
+def auth():
+    return {
+                'u': USER,
+                'p': PASW
+            }
+
 existing = show_grants(GRNT)
-#different = get_different(existing, GRNT)
-print existing
-#missing = get_missing(db, GRNT)
+missing = get_missing(existing, GRNT)
+print json.dumps(existing)
+print json.dumps(missing)
 #
 #if CHK == True:
 #    if not missing:
--- vagrant/scripts/devel_packages.sh
+++ vagrant/scripts/devel_packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y unzip htop tmux git build-essential python-pip python3-pip
+apt-get install -y unzip htop tmux git build-essential python-pip python3-pip jq
 
 pip install influxdb
 pip3 install influxdb
